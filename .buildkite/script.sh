d636e3bbb172710597d174695f981482512d51cd
Nizar Choudhury <31782356+nizarchoudhury@users.noreply.github.com>
2024-11-11 16:06:34 -0500

    commented out pwd and echos

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -2,15 +2,15 @@
 
 name=$(buildkite-agent meta-data get name)
 
-echo "pwd"
-pwd
-echo "cd hello"
+#echo "pwd"
+#pwd
+#echo "cd hello"
 cd hello
-echo "pwd"
-pwd
-echo "go build"
+#echo "pwd"
+#pwd
+#echo "go build"
 go build
-echo "chmod+x hello"
+#echo "chmod+x hello"
 chmod +x hello
-echo "./hello $name"
+#echo "./hello $name"
 ./hello $name
